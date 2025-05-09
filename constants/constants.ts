8cf08cd13fbc47b83f92b89b37ce3657f27f39b1
meanstack9716 <meanstack9@gmail.com>
2025-05-09 18:30:08 +0530

    created generic function for async storage

--- constants/constants.ts
+++ constants/constants.ts
@@ -5,3 +5,4 @@ export const DISCOUNT_PERCENTAGE = 30;
 export const PRODUCT_CODE = 32361470;
 export const PRODUCT_PRICE = "294";
 export const EXTRA_DISCOUNT = "90";
+export const  CART = "guest_cart";
--- utils/cartStorage.ts
+++ utils/cartStorage.ts
@@ -1,29 +1,16 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { CART } from "@/constants/constants";
+import { saveToStorage, getFromStorage, removeFromStorage } from "./storageUtils";
 
-const CART_KEY = "guest_cart";
 
 export const saveCartToStorage = async (cartItems: any[]) => {
-  try {
-    await AsyncStorage.setItem(CART_KEY, JSON.stringify(cartItems));
-  } catch (e) {
-    console.error("Error saving cart to storage", e);
-  }
+  await saveToStorage(CART, cartItems);
 };
 
-export const getCartFromStorage = async () => {
-  try {
-    const data = await AsyncStorage.getItem(CART_KEY);
-    return data ? JSON.parse(data) : [];
-  } catch (e) {
-    console.error("Error reading cart from storage", e);
-    return [];
-  }
+export const getCartFromStorage = async (): Promise<any[]> => {
+  const data = await getFromStorage<any[]>(CART);
+  return data ?? [];
 };
 
 export const clearCartFromStorage = async () => {
-  try {
-    await AsyncStorage.removeItem(CART_KEY);
-  } catch (e) {
-    console.error("Error clearing cart from storage", e);
-  }
+  await removeFromStorage(CART);
 };
