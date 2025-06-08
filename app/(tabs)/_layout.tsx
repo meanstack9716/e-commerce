6c98cbc854b7eaf6621815c982ae7e77acdee82a
meanstack9716 <meanstack9@gmail.com>
2025-06-08 18:07:28 +0530

    fixed folder structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Image, Text, Platform, SafeAreaView, StyleSheet } from "react-native";
 import colors from "@/style/staticColors";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import staticColors from "@/style/staticColors";
-import {fontSizes} from "@/style/typography";
+import { fontSizes } from "@/style/typography";
 export default function TabLayout() {
   const insets = useSafeAreaInsets();
   return (
@@ -28,14 +28,17 @@ export default function TabLayout() {
             }
 
             let iconName: any;
-            if (route.name === "cart")
+            if (route.name === "cart") {
               iconName = focused ? "cart" : "cart-outline";
-            if (route.name === "(profile)")
-              iconName = focused ? "person-outline" : "person-outline";
+            } else if (route.name === "wishlist") {
+              iconName = focused ? "heart" : "heart-outline";
+            } else if (route.name === "(profile)") {
+              iconName = focused ? "person" : "person-outline";
+            }
 
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: colors.primary,
+          tabBarActiveTintColor: colors.primaryBlue,
           tabBarInactiveTintColor: staticColors.textLightGray,
           tabBarStyle: {
             backgroundColor: colors.white,
@@ -44,25 +47,30 @@ export default function TabLayout() {
             paddingBottom: Platform.OS === "ios" ? 0 : 0,
           },
           contentStyle: { backgroundColor: colors.white },
-          tabBarLabel: ({ focused, color }) => (
-            <Text
-              style={{
-                fontFamily: focused ? "HelveticaBold" : "helvetica",
-                fontSize: fontSizes.xs,
-                color,
-              }}
-            >
-              {route.name === "index"
-                ? "Home"
-                : route.name === "cart"
-                ? "Cart"
-                : "Profile"}
-            </Text>
-          ),
+          tabBarLabel: ({ focused, color }) => {
+            let label = "";
+            if (route.name === "index") label = "Home";
+            else if (route.name === "cart") label = "Cart";
+            else if (route.name === "wishlist") label = "Wishlist";
+            else if (route.name === "(profile)") label = "Profile";
+
+            return (
+              <Text
+                style={{
+                  fontFamily: focused ? "HelveticaBold" : "helvetica",
+                  fontSize: fontSizes.xs,
+                  color,
+                }}
+              >
+                {label}
+              </Text>
+            );
+          },
           headerShown: false,
         })}
       >
         <Tabs.Screen name="index" />
+        <Tabs.Screen name="wishlist" />
         <Tabs.Screen name="cart" />
         <Tabs.Screen name="(profile)" />
       </Tabs>
